da88e02addc3baa52ce5445ddf24c825cdfb84ec
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2021-12-02 18:19:00 +0100

    Remove prototype from truc

--- skatik_prod/src/chain.rs
+++
@@ -1,5 +0,0 @@
-pub mod streams {
-    include!(concat!(env!("OUT_DIR"), "/chain_streams.rs"));
-}
-
-include!(concat!(env!("OUT_DIR"), "/chain.rs"));
--- skatik_prod_codegen/src/filter/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod anchor;
-pub mod dedup;
-pub mod fork;
-pub mod group;
-pub mod hof;
-pub mod in_place;
-pub mod sink;
-pub mod sort;
