53b6110bb7e3be80f3942fd91428e74db999a603
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2025-03-13 20:40:15 +0100

    Workaround rustup 1.28 issue

--- examples/target_types/src/main.rs
+++ examples/target_types/src/main.rs
@@ -1,12 +1,29 @@
+use std::io::Write;
+
+use clap::Parser;
 use machin_data::MachinEnum;
 use truc::record::type_resolver::StaticTypeResolver;
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long, value_parser, value_name = "OUTPUT")]
+    output: Option<String>,
+}
+
 fn main() {
+    let args = Args::parse();
+
     let mut type_infos = StaticTypeResolver::new();
 
     type_infos.add_all_types();
 
     type_infos.add_type::<MachinEnum>();
 
-    println!("{}", type_infos.to_json_string_pretty().unwrap());
+    let content = type_infos.to_json_string_pretty().unwrap();
+    if let Some(output) = args.output {
+        let mut output = std::fs::File::create(output).unwrap();
+        writeln!(output, "{}", content).unwrap();
+    } else {
+        println!("{}", content);
+    }
 }
