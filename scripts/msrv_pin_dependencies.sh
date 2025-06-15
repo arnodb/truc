a0b195e3f33a27187025e171150077218540ec6e
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2025-06-15 16:44:20 +0200

    Pin readme dependencies

--- scripts/msrv_pin_dependencies.sh
+++ scripts/msrv_pin_dependencies.sh
@@ -3,6 +3,7 @@
 set -eu
 
 set -x
+
 cargo update -p ppv-lite86 --precise 0.2.17
 cargo update -p derive_more --precise 0.99.17
 cargo update -p either --precise 1.13.0
@@ -11,4 +12,11 @@ cargo update -p libc --precise 0.2.163
 cargo update -p libm --precise 0.2.9
 cargo update -p os_str_bytes --precise 6.1.0
 cargo update -p textwrap --precise 0.16.1
+
+cd examples/readme
+
+cargo update -p derive_more --precise 0.99.17
+cargo update -p either --precise 1.13.0
+
+cd -
 
