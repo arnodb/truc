726565eb736cd8b63276748acb8a79e688fc60b8
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2025-12-29 14:34:45 +0100

    Pin more for MSRV

--- scripts/msrv_pin_dependencies.sh
+++ scripts/msrv_pin_dependencies.sh
@@ -13,12 +13,18 @@ cargo update -p libm --precise 0.2.9
 cargo update -p os_str_bytes --precise 6.1.0
 cargo update -p textwrap --precise 0.16.1
 cargo update -p quote --precise 1.0.40
+cargo update -p proc-macro2 --precise 1.0.101
+cargo update -p ryu --precise 1.0.20
+cargo update -p itoa --precise 1.0.15
 
 cd examples/readme
 
 cargo update -p derive_more --precise 0.99.17
 cargo update -p either --precise 1.13.0
 cargo update -p quote --precise 1.0.40
+cargo update -p proc-macro2 --precise 1.0.101
+cargo update -p ryu --precise 1.0.20
+cargo update -p itoa --precise 1.0.15
 
 cd -
 
